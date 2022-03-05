2615fa2dce4744385b4fb02d26cd89344b8575ec
ddathvhe151280 <99705418+ddathvhe151280@users.noreply.github.com>
2022-03-05 22:24:13 +0700

    updated

---
+++ src/java/dao/DAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author antoan
+ */
+public class DAO {
+    Connection conn = null;
+    PreparedStement ps = null;
+    ResultSet rs = null;
+}
