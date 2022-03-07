310a06320370d71c988991cd513195bbbc2a6e64
ddathvhe151280 <99705418+ddathvhe151280@users.noreply.github.com>
2022-03-07 22:40:10 +0700

    updated

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -43,6 +43,47 @@ public class DAO {
         }
         return list;
     }
+     public List<Product> getProductByCID(String cid) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where cateID = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, cid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+      public Product getProductByID(String id) {
+        String query = "select * from product\n" +
+"where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
 
     public List<Category> getAllCategory() {
         List<Category> list = new ArrayList<>();
@@ -78,16 +119,6 @@ public class DAO {
         } catch (Exception e) {
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        DAO dao = new DAO();
-        List<Product> list = dao.getAllProduct();
-        List<Category> listC = dao.getAllCategory();
-
-        for (Category o : listC) {
-            System.out.println(o);
-        }
     }
 
 }
